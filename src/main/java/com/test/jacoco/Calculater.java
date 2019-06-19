c9b3e9fb1ac44389b54d6894373164309acdc8de
charlyne <nanbiandehe@sina.com>
2019-06-19 16:00:59 +0800

    first commit

---
+++ src/main/java/com/test/jacoco/Calculater.java
@@ -0,0 +1,26 @@
+package com.test.jacoco;/**
+ * Created by didi on 2019/6/17.
+ */
+
+/**
+ * @description:
+ * @author: charlyne
+ * @time: 2019/6/17 4:08 PM
+ */
+public class Calculater {
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+}
---
+++ src/test/java/com/test/jacoco/CalculaterTest.java
@@ -0,0 +1,32 @@
+package com.test.jacoco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by didi on 2019/6/17.
+ */
+public class CalculaterTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, new Calculater().add(1, 4));
+    }
+
+    @Test
+    public void testSuntract() {
+        assertEquals(3, new Calculater().subtract(5, 2));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(10, new Calculater().multiply(5, 2));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(3, new Calculater().divide(6, 2));
+    }
+
+}
