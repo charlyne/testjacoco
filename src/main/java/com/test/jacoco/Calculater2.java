711beb040098264c510b4c4e90dbdf87b56f9304
charlyne <nanbiandehe@sina.com>
2019-06-24 11:48:56 +0800

    to test the flow

--- src/main/java/com/test/jacoco/Calculater2.java
+++ src/main/java/com/test/jacoco/Calculater2.java
@@ -29,4 +29,10 @@ public class Calculater2 {
     public int divid6(int x, int y) {
         return x / y;
     }
+
+    
+    public int add(int x, int y,int z) {
+        return x + y;
+    }
+
 }
